6f52534e75b75222f89328f1c2f30d96cee1f7b7
Shamseer88 <alishamseer@gmail.com>
2023-11-03 12:08:09 +0530

    creating components

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import AppNav from "./AppNAv";
+import Logo from "./Logo";
+import styles from "./Sidebar.module.css";
+export default function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+      <p>List of Cities</p>
+      <footer className={styles.footer}>
+        <p className={styles.copyright}>
+          &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
+        </p>
+      </footer>
+    </div>
+  );
+}
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,9 +1,13 @@
 import AppNav from "../components/AppNAv";
+import Map from "../components/Map";
+import Sidebar from "../components/Sidebar";
+import styles from "./AppLayout.module.css";
 
 export default function AppLayout() {
   return (
-    <div>
-      <AppNav />
+    <div className={styles.app}>
+      <Sidebar />
+      <Map />
     </div>
   );
 }
